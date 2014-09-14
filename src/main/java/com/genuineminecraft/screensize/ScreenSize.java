c93790d01a3665043b3e2120428052747cb0e21d
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2014-09-13 19:35:51 -0700

    Version Sync

--- src/main/java/com/genuineminecraft/screensize/ScreenSize.java
+++ src/main/java/com/genuineminecraft/screensize/ScreenSize.java
@@ -22,7 +22,7 @@ public class ScreenSize {
 
 	public static final String MODID = "ScreenSize";
 	public static final String NAME = "[Genuine] Screen Size";
-	public static final String VERSION = "1.6";
+	public static final String VERSION = "1.7.10-r1";
 	public static KeyBinding screenSizeKey;
 
 	@EventHandler
