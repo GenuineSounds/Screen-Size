8c94f543bfdb1bcbfdd4ae93dea2358a2846b86f
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2015-01-14 22:35:56 -0800

    Orginized Methods

--- src/main/java/com/genuineminecraft/screensize/ScreenSize.java
+++ src/main/java/com/genuineminecraft/screensize/ScreenSize.java
@@ -28,21 +28,20 @@ public class ScreenSize {
 	public static final String VERSION = "1.7.10-r3";
 	public static KeyBinding screenSizeKey;
 
-	@SideOnly(Side.CLIENT)
-	@SubscribeEvent
-	public void keyPress(KeyInputEvent keyInput) {
-		if (screenSizeKey.getIsKeyPressed())
-			Minecraft.getMinecraft().displayGuiScreen(new GuiScreenSize(Minecraft.getMinecraft().currentScreen));
-	}
-
-	@EventHandler
-	public void load(FMLInitializationEvent event) {
-		FMLCommonHandler.instance().bus().register(this);
-	}
-
 	@EventHandler
 	public void pre(FMLPreInitializationEvent event) {
 		screenSizeKey = new KeyBindingScreenSize();
 		ClientRegistry.registerKeyBinding(screenSizeKey);
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		FMLCommonHandler.instance().bus().register(this);
+	}
+
+	@SubscribeEvent @SideOnly(Side.CLIENT)
+	public void keyPress(KeyInputEvent keyInput) {
+		if (screenSizeKey.getIsKeyPressed())
+			Minecraft.getMinecraft().displayGuiScreen(new GuiScreenSize(Minecraft.getMinecraft().currentScreen));
 	}
 }
