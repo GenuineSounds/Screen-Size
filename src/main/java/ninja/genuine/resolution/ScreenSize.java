85cd6a898a157a3da970fea8b24e471b9b4b4c3e
Brian Wiegand <GenuineSoundsLtd@gmail.com>
2016-01-13 04:04:42 -0800

    Complete Refactor

---
+++ src/main/java/ninja/genuine/resolution/ScreenSize.java
@@ -0,0 +1,46 @@
+package ninja.genuine.resolution;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import ninja.genuine.resolution.gui.GuiScreenSize;
+import ninja.genuine.resolution.key.KeyBindingScreenSize;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.InputEvent.KeyInputEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@Mod(modid = ScreenSize.MODID, name = ScreenSize.NAME, version = ScreenSize.VERSION)
+public class ScreenSize {
+
+	@Instance(ScreenSize.MODID)
+	public static ScreenSize instance;
+	public static final String MODID = "ScreenSize";
+	public static final String NAME = "ScreenSize";
+	public static final String VERSION = "1.0.16";
+	public static KeyBinding screenSizeKey;
+
+	@EventHandler
+	public void init(final FMLInitializationEvent event) {
+		FMLCommonHandler.instance().bus().register(this);
+	}
+
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void keyPress(final KeyInputEvent keyInput) {
+		if (ScreenSize.screenSizeKey.getIsKeyPressed())
+			Minecraft.getMinecraft().displayGuiScreen(new GuiScreenSize(Minecraft.getMinecraft().currentScreen));
+	}
+
+	@EventHandler
+	public void pre(final FMLPreInitializationEvent event) {
+		ScreenSize.screenSizeKey = new KeyBindingScreenSize();
+		ClientRegistry.registerKeyBinding(ScreenSize.screenSizeKey);
+	}
+}
---
+++ src/main/java/ninja/genuine/resolution/key/KeyBindingScreenSize.java
@@ -0,0 +1,27 @@
+package ninja.genuine.resolution.key;
+
+import net.minecraft.client.settings.KeyBinding;
+
+import org.lwjgl.input.Keyboard;
+
+public class KeyBindingScreenSize extends KeyBinding {
+
+	public KeyBindingScreenSize() {
+		super("Screen Size", Keyboard.KEY_ADD, "Screen size gui");
+	}
+
+	@Override
+	public String getKeyCategory() {
+		return "Screen Size";
+	}
+
+	@Override
+	public int getKeyCodeDefault() {
+		return Keyboard.KEY_ADD;
+	}
+
+	@Override
+	public String getKeyDescription() {
+		return "Open Screen Size Window";
+	}
+}
